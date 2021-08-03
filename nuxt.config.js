120878703a41cb24b40f3cb794a6e1e5bbd2ee7d
Mark Dunbavan <mdunbavan@gmail.com>
2021-08-03 22:58:00 +0100

    change folder dir
    Signed-off-by: Mark Dunbavan <mdunbavan@gmail.com>

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,7 @@ export default {
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     '@/assets/css/base',
-    'swiper/css/swiper.min.css'
+    'swiper/swiper.min.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
