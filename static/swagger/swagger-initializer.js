f5d9d8652d25ae196675c62abf4a1a818cad0627
Bob Callaway <bcallaway@google.com>
2022-06-29 13:25:12 -0400

    add fulcio openAPI docs
    Signed-off-by: Bob Callaway <bcallaway@google.com>

--- static/swagger/swagger-initializer.js
+++ static/swagger/swagger-initializer.js
@@ -12,7 +12,20 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://raw.githubusercontent.com/sigstore/rekor/main/openapi.yaml",
+    urls: [
+      {
+	      "name": "Rekor",
+	      "url": "https://raw.githubusercontent.com/sigstore/rekor/main/openapi.yaml"
+      },
+      {
+	      "name":"Fulcio",
+	      "url": "https://raw.githubusercontent.com/sigstore/fulcio/main/fulcio.swagger.json"
+      },
+      {
+	      "name":"Fulcio - Legacy API",
+	      "url": "https://raw.githubusercontent.com/sigstore/fulcio/main/fulcio_legacy.swagger.json"
+      }
+    ],
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
