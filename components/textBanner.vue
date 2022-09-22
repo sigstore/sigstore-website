70890cd90e3097aa050e922124049c76b8eb8adb
Trevor Rosen <trevrosen@github.com>
2022-09-22 13:55:07 -0500

    Add Invertocat logo signifying GitHub commitment
    Signed-off-by: Trevor Rosen <trevrosen@github.com>

--- components/textBanner.vue
+++ components/textBanner.vue
@@ -25,6 +25,7 @@
                     <div class="flex items-center justify-between md:justify-start">
                         <img class="md:w-122 w-55 md:mr-20" src="/img/openssf-horizontal-color.svg" alt="OpenSSF sponsor logo" />
                         <img class="md:w-122 w-55 md:mr-20" src="/img/chainguard.svg" alt="Chainguard sponsor logo" />
+                        <img class="md:w-70 w-55 md:mr-20" src="/img/github-logo.svg" alt="GitHub sponsor logo" />
                         <img class="md:w-122 w-55 md:mr-20" src="/img/cisco.png" alt="Cisco sponsor logo" />
                         <img class="md:w-122 w-55 md:mr-20" src="/img/googlelogo.png" alt="Google sponsor logo" />
                         <img class="md:w-122 w-55 md:mr-20" src="/img/hpe.png" alt="HPE sponsor logo" />
