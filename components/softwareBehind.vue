7d59048b4098114f16670d28af1b7a16b81ad997
Hrittik Roy <67012359+hrittikhere@users.noreply.github.com>
2023-04-12 20:10:00 +0000

    Add Policy controller to Product
    Signed-off-by: Hrittik Roy <67012359+hrittikhere@users.noreply.github.com>

--- components/softwareBehind.vue
+++ components/softwareBehind.vue
@@ -3,7 +3,7 @@
     <section class="py-64 md:py-128 md:px-16 bg-white" data-header-text="text-gray-dark">
         <div class="text-gray-dark container text-left">
             <h3 class="text-36 pb-44 text-gray-dark">What's behind the scenes?</h3>
-            <div class="flex flex-row flex-wrap justify-center items-start md:-ml-30 md:-mr-30">
+            <div class="flex flex-row flex-wrap justify-left items-start md:-ml-30 md:-mr-30">
                 <div
                 v-for="(product, index) in software"
                 v-if="product" 
