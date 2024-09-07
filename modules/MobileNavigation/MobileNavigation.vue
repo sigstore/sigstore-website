efa4d00f77f0a2f93e58aeca39aefff9e18544ba
Bob Callaway <bobcallaway@users.noreply.github.com>
2024-09-07 11:59:15 -0400

    add status link to website for public services (#313)
    Signed-off-by: Bob Callaway <bcallaway@google.com>

--- modules/MobileNavigation/MobileNavigation.vue
+++ modules/MobileNavigation/MobileNavigation.vue
@@ -54,6 +54,18 @@
                     </span>
                 </a>
             </li>
+            <li>
+                <a class="footer--nav-button mr-36 flex justify-between text-19 hover:text-purple-light h items-center" :class="[text]" target="_blank" href="https://status.sigstore.dev">
+                    Status
+                    <span class="ml-6 h-20">
+                        <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M18 13V19C18 19.5304 17.7893 20.0391 17.4142 20.4142C17.0391 20.7893 16.5304 21 16 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V8C3 7.46957 3.21071 6.96086 3.58579 6.58579C3.96086 6.21071 4.46957 6 5 6H11" :stroke="[$route.params.slug === 'trust-security' ? '#ffffff' : '#444444']" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path d="M15 3H21V9" :stroke="[$route.params.slug === 'trust-security' ? '#ffffff' : '#444444']" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path d="M10 14L21 3" :stroke="[$route.params.slug === 'trust-security' ? '#ffffff' : '#444444']" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                    </span>
+                </a>
+            </li>
         </ul>
         <ul class="w-full p-60 border-t-2" :class="[bg != 'bg-purple-dark' ? 'border-gray-dark' : 'border-white']">
             <li v-for="(navItem, index) in socialLinks" :key="index" 
