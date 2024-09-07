a09cb0e009568bbc385af913b7514f1166cf865a
Bob Callaway <bcallaway@google.com>
2024-09-07 08:20:30 -0400

    add status link to website for public services
    Signed-off-by: Bob Callaway <bcallaway@google.com>

--- modules/Navigation/Navigation.vue
+++ modules/Navigation/Navigation.vue
@@ -45,6 +45,18 @@
                     </a>
                 </li>
                 <li>
+                    <a class="mr-16 lg:mr-36 flex justify-between items-center" :class="[text]" target="_blank" href="https://status.sigstore.dev">
+                        Status
+                        <span class="ml-6 h-16">
+                            <svg width="15" height="15" viewBox="0 0 24 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M18 13V19C18 19.5304 17.7893 20.0391 17.4142 20.4142C17.0391 20.7893 16.5304 21 16 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V8C3 7.46957 3.21071 6.96086 3.58579 6.58579C3.96086 6.21071 4.46957 6 5 6H11" :stroke="[$route.params.slug === 'trust-security' ? '#ffffff' : '#444444']" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M15 3H21V9" :stroke="[$route.params.slug === 'trust-security' ? '#ffffff' : '#444444']" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M10 14L21 3" :stroke="[$route.params.slug === 'trust-security' ? '#ffffff' : '#444444']" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                        </span>
+                    </a>
+                </li>
+                <li>
                     <a :class="[text]" href="https://github.com/sigstore" target="_blank" title="GitHub profile link">
                         <GitHubLogo :class="[$route.params.slug === 'trust-security' ? 'fill-white' : 'fill-black']" />
                     </a>
